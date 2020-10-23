8bd075761ce5bb8476d19f15a4bfe651ea4b0ed9
Eugênio <einocencio@infraspeak.com>
2020-10-23 12:35:18 +0100

    parse package.json and read dependencies an mount queue object

--- app.js
+++ app.js
@@ -2,24 +2,30 @@
 fs = require('fs');
 var exec = require('child_process').exec;
 
-// extractPackageDependencies();
+let extractedDependencies = extractPackageDependencies();
 
-// function extractPackageDependencies() {
-//     return fs.readFile('./package-mock.json', 'utf8', (err, data) => {
-//         if (err) {
-//             throw Error ('cannot read file')
-//         }
-//         return JSON.parse(data).dependencies
-//     });
-// }
+function extractPackageDependencies() {
+    return new Promise((resolve) => {
+        fs.readFile('./package-mock.json', 'utf8', (err, data) => {
+            if (err) {
+                throw Error('cannot read file')
+            }
+            resolve(JSON.parse(data).dependencies)
+        })
+    });
+}
 
-
+extractedDependencies.then(dependencies => {
+    Object.keys(dependencies).map((key) => {
+        searchForRepositoryInformation(`${key}@${dependencies[key]}`).then(repoInfo => sendToQueue(repoInfo))
+    })
+})
 
 async function searchForRepositoryInformation(repositoryName) {
-    return new Promise((resolve, reject) => {
-        exec(`npm view ${repositoryName} -json`, function (err, stdout, stderr) {
+    return new Promise((resolve) => {
+        exec(`npm view ${repositoryName} -json`, function (err, stdout) {
             if (err) {
-                // should have err.code here?  
+                throw Error('cannot read repository name')
             }
             resolve({
                 name: JSON.parse(stdout).name,
@@ -29,10 +35,23 @@ async function searchForRepositoryInformation(repositoryName) {
     })
 }
 
-searchForRepositoryInformation('axios@0.19.2').then(r => console.log(r))
+function sendToQueue(repoInfo) {
+    let queue = 'REPO_'
 
+    if (repoInfo.url.includes('github.com')) {
+        queue = `${queue}GITHUB.COM`
+    }
 
-function sendToQueue(payload) {
+    if (repoInfo.url.includes('gitlab.com')) {
+        queue = `${queue}GITLAB.COM`
+    }
 
+    if (repoInfo.url.includes('bitbucket.com')) {
+        queue = `${queue}BITBUCKET.COM`
+    }
+
+    return {
+        payload: repoInfo
+    }
 }
 
