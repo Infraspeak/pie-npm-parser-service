88b86dfea0402e11bb2a8bd1404d118417d1e2ae
carlosguedespinto <cguedes@infraspeak.com>
2020-10-23 17:39:28 +0100

    file organization

--- testing/dispatch.js
+++ testing/dispatch.js
@@ -5,13 +5,12 @@ const clientOptions = {
     'port': '6379',
 };
 
+//test sending file to redis
 const publisher = redis.createClient(clientOptions);
 fs.readFile('./package-mock.json', 'utf8', (err, data) => {
     if (err) {
-        throw Error('cannot read file')
+        throw Error('Cannot read file')
     }
-
-    console.log('init');
     publisher.publish("NPM_FILE", JSON.stringify({headers: { websocketId: '1231231'}, payload: JSON.parse(data)}));
     publisher.quit();
 });
